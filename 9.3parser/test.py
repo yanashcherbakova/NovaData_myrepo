9cb9c4aa70c0fa1e567c28c0f41d948b7378f8b5
yanashcherbakova <soloppgango@gmail.com>
2026-02-05 14:11:31 -0500

    hw: chores

--- 9.3parser/test.py
+++ 9.3parser/test.py
@@ -10,43 +10,46 @@ from db_connection import create_tables, load_data
 ## csv structure:
 ## UPC, BOOK_TITLE / price / , rating, / In stock (0, 1, 2...), 
 
+def main():
+    # === parse ... parse ... parse ===
+    url = "https://books.toscrape.com/catalogue/page-1.html"
+    base_url = "https://books.toscrape.com/catalogue/"
 
-# === parse ... parse ... parse ===
-url = "https://books.toscrape.com/catalogue/page-1.html"
-base_url = "https://books.toscrape.com/catalogue/"
+    HEADERS = {
+        "User-Agent": (
+            "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
+            "AppleWebKit/537.36 (KHTML, like Gecko) "
+            "Chrome/120.0.0.0 Safari/537.36"
+        ),
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.9",
+    } 
 
-HEADERS = {
-    "User-Agent": (
-        "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) "
-        "AppleWebKit/537.36 (KHTML, like Gecko) "
-        "Chrome/120.0.0.0 Safari/537.36"
-    ),
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-    "Accept-Language": "en-US,en;q=0.9",
-} 
+    http = HttpClient(HEADERS)
+    pager = CatalogePager()
 
-http = HttpClient(HEADERS)
-pager = CatalogePager()
+    client = BookBowler(http, pager, base_url)
 
-client = BookBowler(http, pager, base_url)
+    parsed_info = client.crawl(url)
 
-parsed_info = client.crawl(url)
+    filename = write_to_csv(parsed_info)
 
-filename = write_to_csv(parsed_info)
+    # === process ... process ... prosecc ===
+    # we'll derive 2 DFs ---> First: df with single books
+    #                    ---> Second: df with volumes
+
+    df = read_csv(filename)
+    df = clean_books_df(df)
+
+    single_df, volume_df = get_volumes_and_single_books(df)
+
+    # === upload ... upload ... upload ===
+
+    engine = create_engine("postgresql+psycopg2://yana:1147@localhost:5432/bookstore")
+
+    create_tables(engine)
+    load_data(engine, single_df, volume_df)
 
 
-# === process ... process ... prosecc ===
-# we'll derive 2 DFs ---> First: df with single books
-#                    ---> Second: df with volumes
-
-df = read_csv(filename)
-df = clean_books_df(df)
-
-single_df, volume_df = get_volumes_and_single_books(df)
-
-# === upload ... upload ... upload ===
-
-engine = create_engine("postgresql+psycopg2://yana:1147@localhost:5432/bookstore")
-
-create_tables(engine)
-load_data(engine, single_df, volume_df)
+if __name__ == "__main__":
+    main()
